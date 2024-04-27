e9718582e10be2b7334bd0e17bc0b02f39d90340
mahmud444t <ferdausemahmud@gmail.com>
2024-04-27 16:59:06 +0600

    getiing single card data

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnxxphb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,6 +36,15 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await collection.findOne(query);
+            res.send(result);
+        })
+
+
         app.post('/items', async (req, res) => {
             const newitem = req.body;
             console.log(newitem)
